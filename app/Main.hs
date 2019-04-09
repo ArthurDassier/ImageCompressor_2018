bd93454dfc70ce7c1e44060cad0136494707dc9b
Arthur Dassier <arthur.dassier@epitech.eu>
2019-04-09 18:28:42 +0200

    Clean

--- app/Main.hs
+++ app/Main.hs
@@ -7,29 +7,39 @@ import Debug.Trace
 import Text.Read
 import Data.List
 
--- type Point (Double, Double)
--- type Color (Double, Double, Double)
+replaceStringSecond :: [String] -> Int -> [String] -> [String]
+replaceStringSecond array index result
+    | (length array) == index = result
+    | otherwise = replaceStringSecond array (index + 1) (result ++ [((init (array !! index)) ++ [']'])])
 
--- data Image = Image Int, Int, Int, Int, Int deriving (show)
--- square :: Int -> Int -> Double
--- square a b = ((a - b) ^ 2)
-
-isParenthesis :: Char -> Bool
-isParenthesis c
-    | c == '(' || ord c == ')' = True
-    | otherwise = False
-
-    -- replaceO = map (\c -> if c=='O' then 'X'; else c)
+replaceStringFirst :: [String] -> Int -> [String] -> [String]
+replaceStringFirst array index result
+    | (length array) == index = replaceStringSecond result 0 []
+    | otherwise = replaceStringFirst array (index + 1) (result ++ [(['['] ++ (tail (array !! index)))])
 
 split :: String -> Int -> String -> [String] -> [String]
 split [] _ _ _ = []
-split _ 180 _ _ = []
 split file index str list
-    | (length file - 1) == index = list
+    | (length file) == index = list
     | (file !! index) /= '\n' = split file (index + 1) (str ++ [(file !! index)]) list
     | otherwise = split file (index + 1) [] (list ++ [str])
+
+replaceChar :: Char -> Char -> Char -> Char
+replaceChar c toReplace new
+    | c == toReplace = new
+    | otherwise = c
+
+replaceString :: String -> String -> Char -> Char -> Int -> String
+replaceString array result c1 c2 index
+    | 0 == index = result
+    | otherwise = replaceString (tail array) (result ++ [(replaceChar (head array) c1 c2)]) c1 c2 (index - 1)
+
+replaceArray :: [String] -> [String] -> Char -> Char -> Int -> [String]
+replaceArray array result c1 c2 index
+    | (length array) == 0 = result
+    | otherwise = replaceArray (tail array) (result ++ [(replaceString (head array) [] c1 c2 (length (head array)))]) c1 c2 (index + 1)
 
 main = do  
     handle <- openFile "example.txt" ReadMode
     contents <- hGetContents handle
-    print (split contents 0 [] [])
+    print (replaceArray (replaceArray (replaceArray (replaceStringFirst (split contents 0 [] []) 0 []) [] ' ' ',' 0) [] ')' ' ' 0) [] '(' ' ' 0)
