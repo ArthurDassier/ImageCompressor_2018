b3ec99cbc7d227df73e187de92b7861d85de30e4
Charles Raimbault <charles.raimbault@epitech.eu>
2019-04-17 17:24:51 +0200

    Commit before changement

--- app/Main.hs
+++ app/Main.hs
@@ -12,32 +12,6 @@ import Parsing
 import Algorithm
 import Extract
 
-getRGB:: Centroid -> Color -> [Pixel] -> Int -> Double -> Color
-getRGB centroid (sumR, sumG, sumB) array index number
-    | length (array) == index = trace (show (number))((sumR / number), (sumG / number), (sumB / number))
-    | centroid == (pixelCentroid (array !! index)) = getRGB centroid (sumR + pixelR (array !! index), sumG + pixelG (array !! index), sumB + pixelB (array !! index)) array (index + 1) (number + 1)
-    | otherwise = getRGB centroid (sumR, sumG, sumB) array (index + 1) (number)
-
--- deleteIndex :: [Centroid] -> Int -> Int -> [Centroid] -> [Centroid]
--- deleteIndex array todelete index result
---     | index == 0 = result
---     | index == todelete = deleteIndex array todelete (index - 1) result
---     | otherwise = deleteIndex array todelete (index - 1) (result ++ (array !! index))
-
-getRandomPixel :: [Pixel] -> Int -> Pixel
-getRandomPixel array n = (array !! n)
-
-
-fromRandomToCendroid :: Int -> [Pixel] -> Int -> [Centroid] -> [Centroid]
-fromRandomToCendroid n struct 0 array = array
-fromRandomToCendroid n struct idx array = fromRandomToCendroid (n + 1) struct (idx - 1) ([(makeCentroid (getRandomPixel struct n))] ++ array)
-
-createNewCentroid :: Int -> [Pixel] -> [Centroid] -> [Centroid] -> [Centroid]
-createNewCentroid (-1) pixels centroid result = result
-createNewCentroid index pixels centroid result = do
-    let newRGB = getRGB (centroid !! index) (0, 0, 0) pixels 0 0
-    createNewCentroid (index - 1) pixels centroid (result ++ [(Centroid (centroidX (centroid !! index)) (centroidY (centroid !! index)) (fstt newRGB) (sndt newRGB) (endt newRGB))])
-
 main = do
     arg <- getArgs
     handle <- openFile "example.txt" ReadMode
@@ -45,8 +19,5 @@ main = do
     let struct = (makeStruct (words contents) [])
     let centroid = fromRandomToCendroid 0 struct 3 []
     let test = findNearestCentroid 0 struct [] centroid
-    -- print
-    -- print test
-    -- print ((centroid !! 0))
-    print "ok"
-    -- print (getRGB (centroid !! 0) (0, 0, 0) test 0 0)
+    print (run [] centroid test)
+    -- print "a"
--- src/Algorithm.hs
+++ src/Algorithm.hs
@@ -3,11 +3,43 @@
 -- File description:
 -- Algorithm
 
-module Algorithm (findNearestCentroid) where
+module Algorithm where
 
 import Extract
 import Parsing
 
+getRGB:: Centroid -> Color -> [Pixel] -> Int -> Double -> Color
+getRGB centroid (sumR, sumG, sumB) array index number
+    | length (array) == index = ((sumR / number), (sumG / number), (sumB / number))
+    | centroid == (pixelCentroid (array !! index)) = getRGB centroid (sumR + pixelR (array !! index), sumG + pixelG (array !! index), sumB + pixelB (array !! index)) array (index + 1) (number + 1)
+    | otherwise = getRGB centroid (sumR, sumG, sumB) array (index + 1) (number)
+
+getRandomPixel :: [Pixel] -> Int -> Pixel
+getRandomPixel array n = (array !! n)
+
+
+fromRandomToCendroid :: Int -> [Pixel] -> Int -> [Centroid] -> [Centroid]
+fromRandomToCendroid n struct 0 array = array
+fromRandomToCendroid n struct idx array = fromRandomToCendroid (n + 1) struct (idx - 1) ([(makeCentroid (getRandomPixel struct n))] ++ array)
+
+createNewCentroid :: Int -> [Pixel] -> [Centroid] -> [Centroid] -> [Centroid]
+createNewCentroid (-1) pixels centroid result = result
+createNewCentroid index pixels centroid result = do
+    let newRGB = getRGB (centroid !! index) (0, 0, 0) pixels 0 0
+    createNewCentroid (index - 1) pixels centroid (result ++ [(Centroid (centroidX (centroid !! index)) (centroidY (centroid !! index)) (fstt newRGB) (sndt newRGB) (endt newRGB))])
+
+checkEnd :: [Centroid] -> [Centroid] -> Int -> Double -> Bool
+checkEnd [] _ _ _ = False
+checkEnd _ [] _ _ = False
+checkEnd before after index c = True
+    -- | index == (length (before)) = True
+    -- | (centroidR (after !! index)) - (centroidR (before !! index)) > c || (centroidG (after !! index)) - (centroidG (before !! index)) > c || (centroidB (after !! index)) - (centroidB (before !! index)) > c = False
+    -- | otherwise = checkEnd before after (index + 1) c
+
+run :: [Centroid] -> [Centroid] -> [Pixel] -> [Centroid]
+run before after pixel
+    | (checkEnd before after 0 0.3) == True = after
+    | otherwise = run after (createNewCentroid (length after) (findNearestCentroid 0 pixel [] (fromRandomToCendroid 0 pixel 3 [])) after []) pixel
 
 distance :: Color -> Color -> Double
 distance (ux, uy, uz) (vx, vy, vz) = sqrt (((ux - vx) ^ 2) + ((uy - vy) ^ 2) + ((uz - vz) ^ 2))
